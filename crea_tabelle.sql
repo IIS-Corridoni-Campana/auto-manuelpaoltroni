7b5132b15639ba447bbb44f1a48f82a17b8011be
manuelpaoltroni <manuel.paoltroni@corridoni-campana.edu.it>
2026-05-29 08:44:25 +0000

    Lavoro M P

--- crea_tabelle.sql
+++ crea_tabelle.sql
@@ -1,1 +1,58 @@
--- USA QUESTO FILE PER CREARE LE TABELLE DEL DATABASE
+CREATE TABLE Costruttore
+(
+  id INT NOT NULL,
+  nome VARCHAR NOT NULL,
+  nazione VARCHAR NOT NULL,
+  citta VARCHAR NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE Automobile
+(
+  id INT NOT NULL,
+  modello VARCHAR NOT NULL,
+  anno_modello DATE NOT NULL,
+  nazione VARCHAR NOT NULL,
+  cilindrata INT NOT NULL,
+  alimentazione VARCHAR NOT NULL,
+  targa CHAR NOT NULL,
+  tipo_auto VARCHAR NOT NULL,
+  disponibile INT NOT NULL,
+  co_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (co_id) REFERENCES Costruttore(id)
+);
+
+CREATE TABLE Patente
+(
+  data_rilascio DATE NOT NULL,
+  data_scadenza DATE NOT NULL,
+  tipo VARCHAR NOT NULL,
+  id INT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE Cliente
+(
+  id INT NOT NULL,
+  nome VARCHAR NOT NULL,
+  cognome VARCHAR NOT NULL,
+  data_nascita DATE NOT NULL,
+  cod_fiscale CHAR NOT NULL,
+  p_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (p_id) REFERENCES Patente(id)
+);
+
+CREATE TABLE noleggio
+(
+  id INT NOT NULL,
+  data_inizio DATE NOT NULL,
+  data_fine DATE NOT NULL,
+  costo FLOAT NOT NULL,
+  cl_id INT NOT NULL,
+  a_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (cl_id) REFERENCES Cliente(id),
+  FOREIGN KEY (a_id) REFERENCES Automobile(id)
+);
--- inserisci_record.sql
+++ inserisci_record.sql
@@ -1,1 +1,18 @@
 -- USA QUESTO FILE PER INSERIRE I DATI NEL DATABASE
+INSERT INTO Costruttore
+VALUES ("1", "Fiat", "Italia", "Torino")
+
+INSERT INTO Automobile
+VALUES ("1", "Panda", "2017", "Italia", "1600", "Benzina", "FH890CU", "Citycar", True)
+
+INSERT INTO Patente
+VALUES ("2018", "2028", "B")
+
+INSERT INTO Cliente
+VALUES ("1", "Giacomo", "Rossi", "1993", "RSSMRA85A01H501K")
+
+INSERT INTO noleggio
+VALUES ("1", "2026", "2026", "210")
+
+
+
